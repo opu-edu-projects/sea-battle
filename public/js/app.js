fda10bee352ab87f7e3c5064974035356c9a48ab
denis-krasovskyi <deniskrasovskii@gmail.com>
2015-09-12 17:59:06 +0300

    client initial structure

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by admin on 12.09.2015.
+ */
+'use strict';
+
+var app = angular.module('app', [
+    'ngRoute',
+
+    'roomsController',
+    'roomService'
+]);
+
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/room', {
+                controller: 'RoomList'
+            });
+    }]);
